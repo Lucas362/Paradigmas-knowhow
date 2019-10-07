81db972743f8a18a5fe1917c4dca7329fcd40e6e
Lucas362 <lucasvitorifg@gmail.com>
2019-10-07 11:32:04 -0300

    Finishing progenitor

--- progenitor.pl
+++ progenitor.pl
@@ -5,4 +5,21 @@ progenitor(bob,ann).
 progenitor(bob,pat).
 progenitor(pat,jim).
 
+mulher(pam).
+mulher(liz).
+mulher(pat).
+mulher(ann).
+homem(tom).
+homem(bob).
+homem(jim).
+
+diferente(X,Y):-X\==Y.
+
 filho(Y,X):-progenitor(X,Y).
+avos(X,Z):-progenitor(X,Y),progenitor(Y,Z).
+mae(X,Y):-progenitor(X,Y),mulher(X).
+irma(X,Y):-progenitor(Z,X),progenitor(Z,Y),mulher(X),diferente(X,Y).
+tia(X,Y):-progenitor(Z,Y),irma(X,Z).
+
+antepassado(X,Z):-progenitor(X,Z).
+antepassado(X,Z):-progenitor(X,Y),antepassado(Y,Z).
